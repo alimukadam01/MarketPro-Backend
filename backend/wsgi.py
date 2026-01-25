17d73da71a1f566e441345c2c9e4bc165dd22997
Ali Murtaza Mukadam <amurtaza907@gmail.com>
2026-01-25 21:26:48 +0500

    added wsgi config

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/MarketPro/MarketPro-Backend/venv/lib/python3.12/site-packages/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
